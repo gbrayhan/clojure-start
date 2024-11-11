c43dc16ce5961d25ebb209b0fc2b2c4f3cf4475a
Alex Guerrero <gbrayhan@gmail.com>
2024-11-11 17:22:16 -0600

    adding

--- mi-api-rest/project.clj
+++ mi-api-rest/project.clj
@@ -5,7 +5,7 @@
                  [ring/ring-defaults "0.3.3"]
                  [metosin/reitit "0.5.15"]
                  [org.postgresql/postgresql "42.3.1"]
-                 [seancorfield/next.jdbc "1.4.794"]
+                 [seancorfield/next.jdbc "1.2.659"]
                  [migratus "1.3.6"]
                  [cheshire "5.10.0"]]
   :main ^:skip-aot mi-api-rest.core
